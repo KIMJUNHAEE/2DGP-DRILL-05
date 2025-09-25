cc8a75a70af6f0bbd0b8e956319367c6246976f3
KIMJUNHAEE <kkjjhh528@naver.com>
2025-09-25 18:35:45 +0900

    게임루프 완성

---
+++ move_my_character_with_key.py
@@ -0,0 +1,18 @@
+from pico2d import *
+
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+running = True
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+frame = 0
+dir = 0
+speed = 0
+
+
+
+
+while True:
+
+
+
+    pass
